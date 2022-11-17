15c69271bd89ef2731947a7c6735fdcfe9a55764
abhi7980 <86182925+abhi7980@users.noreply.github.com>
2022-11-17 14:29:19 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,52 @@
+$ (".ind1").mouseenter(function(){
+    $("#custom-hide1").show();
+});
+
+$ (".ind1").mouseleave(function(){
+    $("#custom-hide1").hide();
+});
+
+$ (".ind2").mouseenter(function(){
+    $("#custom-hide2").show();
+});
+
+$ (".ind2").mouseleave(function(){
+    $("#custom-hide2").hide();
+});
+
+$ (".ind3").mouseenter(function(){
+    $("#custom-hide3").show();
+});
+
+$ (".ind3").mouseleave(function(){
+    $("#custom-hide3").hide();
+});
+
+$ (".ind4").mouseenter(function(){
+    $("#custom-hide4").show();
+});
+
+$ (".ind4").mouseleave(function(){
+    $("#custom-hide4").hide();
+});
+
+$ (".ind5").mouseenter(function(){
+    $("#custom-hide5").show(); 
+});
+
+$ (".ind5").mouseleave(function(){
+    $("#custom-hide5").hide();
+});
+
+$ (".ind6").mouseenter(function(){
+    $("#custom-hide6").show();
+});
+
+$ (".ind6").mouseleave(function(){
+    $("#custom-hide6").hide();
+});
+
+
+
+
+
